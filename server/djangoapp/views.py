c630bf3b3b14bdfee5d9c32af499faf62d03687d
LeegoWang <onohate@gmail.com>
2024-08-27 04:12:37 -0400

    chore: format code

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -125,7 +125,7 @@ def add_review(request):
             return JsonResponse({"status": 200})
         except Exception:
             return JsonResponse({
-                "status": 401, 
+                "status": 401,
                 "message": "Error in posting review"
             })
     else:
@@ -140,6 +140,6 @@ def get_cars(request):
     car_models = CarModel.objects.select_related("car_make")
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel": car_model.name, 
+        cars.append({"CarModel": car_model.name,
                      "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels": cars})
